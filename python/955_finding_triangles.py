d52cc2539ad154a854e580a07124380231be147a
Bar Angel <bar.ang16@gmail.com>
2026-06-13 20:01:03 +0300

    a solution

--- python/955_finding_triangles.py
+++ python/955_finding_triangles.py
@@ -1,57 +1,52 @@
 from project_euler import validation, solution, Testing, Progress, Measure
+from sympy import divisors
 from math import sqrt, isqrt
-
-def reverse_triangle(k):
-    v = 8 * k + 1
-    r = isqrt(v)
-
-    if r * r != v:
-        return None
-
-    return (r-1)//2
 
 
 def triangle(m):
     return m * (m+1) // 2
 
-def get_next_triangle(t):
-    counter = 1
-    pos = 1
-    #import pdb; pdb.set_trace()
-    rev = None
-    while not rev:
-        res = triangle(counter) + t*counter
-        rev = reverse_triangle(res)
-        counter += 1
-        pos += 1
-    return pos, (counter-1)+t, counter-1
+def find_next_triangle(b):
+    a = -1
+    n = -1
+    optv = -1
+    divs = divisors(8*triangle(b))
+    divs = [d for d in divs if d < 2*b]
+    for v in divs:
+        if 8*triangle(b) <= v*(v+2):
+            break
+        if (8*triangle(b)) % v != 0:
+            continue
 
+        if (8*triangle(b) - v*(v+2)) % (4*v) != 0:
+            continue
+        if (8*triangle(b) + v*(v-2)) % (4*v) != 0:
+            continue
 
-def prod_series_brute_force(size):
-    t = 3
-    counter = 1
-    series = []
+        _a = (8*triangle(b) - v*(v+2))//(4*v)
+        if a < 0 or _a < a:
+            a = _a
+            n = (8*triangle(b) + v*(v-2))//(4*v)
+            optv = v
+    return a, n
 
-    for i in range(size):
-        series.append(t)
-        t += counter
-        if reverse_triangle(t) is not None:
-            counter = 1
-        else:
-            counter += 1
+def solve_single(lim):
+    tri_idx = 3
+    n_idx = 2
+    for _, i in Progress(range(lim-2)):
+        a, tri_idx = find_next_triangle(tri_idx)
+        n_idx += a
 
-    return series
+    return n_idx
 
 @validation
 def validate():
-    ser = prod_series_brute_force(500)
-    print("\n".join([f"{v}=T{reverse_triangle(v) or "~"}" for v in ser])) 
+    Testing.equals(2, solve_single, 2)
+    Testing.equals(7, solve_single, 3)
+    Testing.equals(12, solve_single, 4)
+    Testing.equals(2964, solve_single, 10)
 
-    t = 3
-    pos = 0
-    diff = 0
-    for i in range(70):
-        print(f"{i+1}.\ta{pos} =\tT{t} (+T{diff}) =\t{triangle(t)} (+{triangle(diff)})")
-        m, t, diff = get_next_triangle(t)
-        pos += m
 
+@solution
+def solve():
+    return solve_single(70)
